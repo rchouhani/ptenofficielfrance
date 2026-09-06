66dbdc2e8236f102e64b33b89ac8d70110a6a2c5
rchouhani <rchouhani@me.com>
2026-09-06 15:34:50 +0200

    refactor: update layout structure by adding Navbar and Footer components, and change language to French

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +20,14 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+ return (
+    <html lang="fr" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <div className="stripe-bar" aria-hidden="true" />
+        <Navbar />
+          {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Footer from "./component/Footer";
-import Navbar from "./component/Navbar";
-
 export default function Home() {
   return (
-   <>
-    <Navbar />
-    <Footer />
-   </>
+<><p>page d'accueil</p></>
   );
 }
